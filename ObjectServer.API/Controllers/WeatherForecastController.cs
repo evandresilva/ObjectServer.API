af773b9a1b423e07269f01f4a0db51a134068077
Evandre Da Silva <evandresilva01@gmail.com>
2022-05-16 09:55:27 +0100

    Add chache

--- ObjectServer.API/Controllers/WeatherForecastController.cs
+++ ObjectServer.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ObjectServer.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+             "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
